906e598c6b9101aac0e9578044a47d61e871d855
RuslanGon <ruslan441006@gmail.com>
2025-05-25 15:16:38 +0300

    wer

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import useState from 'react'
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setIsOpen((prevState) => !prevState);
+  };  
+
   return (
-    <div>Navbar</div>
+    <AppBar position="static">
+    <Toolbar>
+      <IconButton
+        color="inherit"
+      >
+        <MenuIcon />
+      </IconButton>
+      <List>
+          <ListItem  disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+      </List>
+
+    </Toolbar>
+  </AppBar>
   )
 }
 
