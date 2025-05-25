19ebe1026653088c76eb9839df48e2b0ca3282bb
RuslanGon <ruslan441006@gmail.com>
2025-05-25 15:27:58 +0300

    wer

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import useState from 'react'
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Container,
+  Slide,
+  useScrollTrigger
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
-
   const [isOpen, setIsOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setIsOpen((prevState) => !prevState);
-  };  
+  };
+
+  // Скрытие AppBar при скролле
+  const trigger = useScrollTrigger({
+    target: typeof window !== "undefined" ? window : undefined,
+  });
 
   return (
-    <AppBar position="static">
-    <Toolbar>
-      <IconButton
-        color="inherit"
-      >
-        <MenuIcon />
-      </IconButton>
-      <List>
-          <ListItem  disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-      </List>
+    <Slide appear={false} direction="down" in={!trigger}>
+      <AppBar position="sticky">
+        <Container maxWidth="lg">
+          <Toolbar>
+            <IconButton color="inherit" onClick={handleDrawerToggle}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              anchor="left"
+              open={isOpen}
+              onClose={handleDrawerToggle}
+            >
+              <List sx={{ width: 250 }}>
+                <ListItem disablePadding>
+                  <ListItemButton sx={{ textAlign: "center" }}>
+                    <ListItemText primary="Фильмы" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Drawer>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Slide>
+  );
+};
 
-    </Toolbar>
-  </AppBar>
-  )
-}
-
-export default Navbar
+export default Navbar;
