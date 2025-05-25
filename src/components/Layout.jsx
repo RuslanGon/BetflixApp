e5a71d3a65de37f7b45e9f4f4267f3fda16a7ba2
RuslanGon <ruslan441006@gmail.com>
2025-05-25 15:49:37 +0300

    wer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../../Navbar.jsx";
+import Navbar from "../components/Navbar.jsx";
 import Footer from "./Footer.jsx";
 import { Container } from "@mui/material";
 
