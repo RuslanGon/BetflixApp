4d2bc47d03b551c10c17e1cc510892e92f60126f
RuslanGon <ruslan441006@gmail.com>
2025-05-29 11:41:14 +0300

    wer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,74 +1,112 @@
-import React, { useState } from "react";
-import MovieIcon from "@mui/icons-material/Movie";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Brightness4, Brightness7 } from '@mui/icons-material';
+import MenuIcon from '@mui/icons-material/Menu';
 import {
   AppBar,
-  Toolbar,
-  IconButton,
+  Box,
+  Container,
+  Divider,
   Drawer,
+  IconButton,
+  Link,
   List,
   ListItem,
   ListItemButton,
-  ListItemText,
   ListItemIcon,
-  Container,
+  ListItemText,
   Slide,
+  Stack,
+  Toolbar,
+  Typography,
   useScrollTrigger,
-  Box,
-  Typography
-} from "@mui/material";
-import { Link } from "react-router-dom";
+} from '@mui/material';
+import React, { useContext, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+import { iconComponents, MOVIE_LISTS, TOP_LISTS } from '../constants.js';
+import { ColorModeContext } from '../context/ToggleColorMode.jsx';
+import Search from './Search.jsx';
 
-  const handleDrawerToggle = () => {
-    setIsOpen((prev) => !prev);
-  };
+
+const Icon = ({ iconName }) => {
+  const IconComponent = iconComponents[iconName];
+  return <IconComponent />;
+};
+
+export default function Navbar() {
+  const [isOpen, setOpen] = useState(false);
+  const { toggleColorMode, mode } = useContext(ColorModeContext);
 
   const trigger = useScrollTrigger({
-    target: typeof window !== "undefined" ? window : undefined,
+    target: window,
   });
+
+  const handleDrawerToggle = () => {
+    setOpen(prevState => !prevState);
+  };
 
   return (
     <Slide appear={false} direction="down" in={!trigger}>
-      <AppBar position="sticky">
+      <AppBar>
         <Container maxWidth="lg">
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2 }}>
+          <Toolbar disableGutters>
+            <IconButton color="inherit" onClick={handleDrawerToggle}>
               <MenuIcon />
             </IconButton>
-            <Typography
-              component={Link}
-              to="/"
-              variant="h4"
-              color="inherit"
-              sx={{ textDecoration: "none", flexGrow: 1 }}
-            >Betflix
-            </Typography>
-            <Drawer anchor="left" open={isOpen} onClose={handleDrawerToggle}>
-              <Box sx={{ width: 250 }} role="presentation" onClick={handleDrawerToggle}>
+            <Drawer open={isOpen} onClose={handleDrawerToggle}>
+              <Box sx={{ width: 250 }} onClick={handleDrawerToggle}>
                 <List>
-                  <ListItem disablePadding>
-                    <ListItemButton component={Link} to="/movie">
-                      <ListItemIcon>
-                        <MovieIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Фильмы" />
-                    </ListItemButton>
-                  </ListItem>
+                  {TOP_LISTS.map(item => (
+                    <Link key={item.title} component={RouterLink} to={item.url}>
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemIcon>
+                            <Icon iconName={item.icon} />
+                          </ListItemIcon>
+                          <ListItemText primary={item.title} />
+                        </ListItemButton>
+                      </ListItem>
+                    </Link>
+                  ))}
+                </List>
+                <Divider />
+                <List>
+                  {MOVIE_LISTS.map(item => (
+                    <Link key={item.title} component={RouterLink} to={item.url}>
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemIcon>
+                            <Icon iconName={item.icon} />
+                          </ListItemIcon>
+                          <ListItemText primary={item.title} />
+                        </ListItemButton>
+                      </ListItem>
+                    </Link>
+                  ))}
                 </List>
               </Box>
             </Drawer>
+            <Stack
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+              width="100%"
+            >
+              <Typography
+                sx={{ color: 'white', textDecoration: 'none' }}
+                component={RouterLink}
+                variant="h4"
+                to="/"
+              >
+                betflix
+              </Typography>
+              <Search />
+              <IconButton color="inherit" onClick={toggleColorMode}>
+                {mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+              </IconButton>
+            </Stack>
           </Toolbar>
         </Container>
       </AppBar>
     </Slide>
   );
-};
-
-export default Navbar;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import CssBaseline from '@mui/material/CssBaseline';
+import ToggleColorMode from './context/ToggleColorMode.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ToggleColorMode>
     <BrowserRouter>
     <CssBaseline />
     <App />
     </BrowserRouter>
+    </ToggleColorMode>
   </StrictMode>,
 )
